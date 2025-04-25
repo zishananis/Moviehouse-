892f9152ea584ea01607ba7376a616193db5e1d7
zishananis <zishananis3@gmail.com>
2025-04-25 11:13:17 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -113,7 +113,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ https://t.me/teamfilmsexpert </b>"""
+    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ https://t.me/filmsexpert_1 Owner - @zishananis_official </b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
