16683568e03dd660f924bce4b90b82575d761400
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-05-25 11:14:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7872783969:AAFd4Z86R0PXua9dxV8epeYdsJpEbT_
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5446981726').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/zishananis_official") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "@zishananis_official") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ' -1002340773096'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/filmsexpert_1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', ' -1002445219066').split()]
