1fe3ed238eb9897554eaaf0c7713419707cd579c
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-05-25 12:10:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1428,8 +1428,9 @@ async def auto_filter(client, msg, spoll=False , pm_mode = False):
         chat_id = message.chat.id
         settings = await get_settings(chat_id , pm_mode=pm_mode)
         searching_msg = await msg.reply_text(f'🔎 sᴇᴀʀᴄʜɪɴɢ {search}')
-        files, offset, total_results = await get_search_results(search)
+	await asyncio.sleep(0.5)
         await searching_msg.delete()
+        files, offset, total_results = await get_search_results(search)
         if not files:
             if settings["spell_check"]:
                 ai_sts = await msg.reply_text(f'𝑶𝑼𝑹 𝑨𝑰 𝑰𝑺 𝑪𝑯𝑬𝑪𝑲𝑰𝑵𝑮 𝒀𝑶𝑼𝑹 𝑺𝑷𝑬𝑳𝑳𝑰𝑵𝑮...')
