5a3be324f76afe835017ac6733bc3caa5eec8955
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-06-14 08:08:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -105,7 +105,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "sharp-april-zishananis-cb465703.koyeb.app")
+URL = environ.get("FQDN", "")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
